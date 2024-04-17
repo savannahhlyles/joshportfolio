6494aa983e720ace92ac4b67806eb5dd7ef9a176
Savannah Lyles <savannahhlyles513@gmail.com>
2024-04-17 15:34:31 -0400

    Home page adds

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,18 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-container">
-      <h1>Joshua Belk</h1>
-      <p className="tagline">
-        Musician Poet Storyteller
-      </p>
+        <h1>Joshua Belk</h1>
+        <div className="tagline">
+          <p>
+            MUSICIAN
+          </p>
+          <p>
+            POET
+          </p>
+          <p>
+            STORYTELLER
+          </p>
+        </div>
     </div>
   );
 }
