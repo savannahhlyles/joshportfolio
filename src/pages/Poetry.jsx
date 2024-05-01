b84f9b074eda56992c53149dc9a8ec63942d0ae1
Savannah Lyles <savannahhlyles513@gmail.com>
2024-05-01 15:47:23 -0400

    Updates

--- src/pages/Poetry.jsx
+++ src/pages/Poetry.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import './Poetry.css'; // Import the CSS file
+import poetryImg from "/images/poetry.png";
 
 function Poetry() {
   return (
-    <div className="poetry-container">
-      <div className="title">
-        <h1>Poetry</h1>
-      </div>
-        <div className="iframe-container">
-          <iframe
-            src="https://www.canva.com/design/DAGDj3LwGdc/tEgWtTmlmnHzphsuuJjs3Q/view?embed"
-            title="Poetry Slide Deck"
-            allowFullScreen
-            className="custom-iframe"
-          ></iframe>
-        </div>
-        <a href="https://www.canva.com/design/DAGDj3LwGdc/tEgWtTmlmnHzphsuuJjs3Q/view?utm_content=DAGDj3LwGdc&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener noreferrer">
-        </a>
-        {' '}
-      </div>
   );
 }
 
