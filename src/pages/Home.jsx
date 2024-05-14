585264eca40f777febef45b45b45e53b1f78d9d5
Savannah Lyles <savannahhlyles513@gmail.com>
2024-05-13 21:46:58 -0400

    Home page animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-// Home.js
-import React from 'react';
+import React, { useEffect } from 'react'; // Import useEffect from React
+
 import './Home.css'; 
 import headerImg from "/images/background.jpg";
 import leaf1 from "/images/leaf1.png";
@@ -8,14 +8,22 @@ import leaf3 from "/images/leaf3.png";
 import linkedin from "/images/linkedin.png";
 import phone from "/images/phone.png";
 import email from "/images/email.png";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Home() {
+  useEffect(() => {
+    AOS.init({
+      // Global settings...
+    });
+  }, []);
+
   return (
     <div className="outerbox">
 
         <div className="image-container">
         <img src={headerImg} alt="Header Image" />
-          <div className="text-container">
+          <div className="text-container" >
             <div className="name-container">
                 <h1>Joshua Belk</h1>
             </div>
@@ -32,21 +40,21 @@ function Home() {
 
         <div className="bio-container">
           <div className="bio">
-            <p id="bio1">
+            <p id="bio1" data-aos="fade-right">
                 I am currently pursuing a degree in sound design
                 <br></br>with a minor in music production at the
                 <br></br><a href="https://www.scad.edu/" target="_blank" id="scad"> Savannah College of Art and Design.</a> 
                 <br></br>Through this program, I am actively refining
                 <br></br>my expertise in the field.
             </p>
-            <p id="bio2">
+            <p id="bio2" data-aos="fade-left">
                 I've crafted songs and poetry since childhood, 
                 <br></br>drawing inspiration from my surroundings. Now, I'm 
                 <br></br><span id="emphasis">recording and mixing,</span>
                 <br></br>blending melodies with guitar and banjo, 
                 <br></br>reflecting natural motifs with metaphorical depth.
             </p>
-            <p id="bio3">
+            <p id="bio3" data-aos="fade-right">
                 Join me on this journey as we explore the realms of
                 <br></br> <span id="emphasis">artistry and expression</span>
                 <br></br>together.
@@ -55,13 +63,13 @@ function Home() {
             <img src={leaf2} alt="Leaf" id="leaf2"/>
             <img src={leaf3} alt="Leaf" id="leaf3"/>
 
-            <a href="tel:+8035773092">
+            <a href="tel:+8035773092" data-aos="fade-up">
             <img src={phone} alt="Phone" id="phone"/>
             </a>
-            <a href="mailto:joshuabelk1@icloud.com">
+            <a href="mailto:joshuabelk1@icloud.com" data-aos="fade-up">
             <img src={email} alt="Email" id="email"/>
             </a>
-            <a href="https://www.linkedin.com/in/joshua-belk-6907aa299/" target="_blank">
+            <a href="https://www.linkedin.com/in/joshua-belk-6907aa299/" target="_blank" data-aos="fade-up">
             <img src={linkedin} alt="LinkedIn" id="linked"/>
             </a>
           </div>
